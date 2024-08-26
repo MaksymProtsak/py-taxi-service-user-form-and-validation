d366d181425ca07a8ccc8582dbac93a31321d934
Maksym Protsak <maksym.protsak@gmail.com>
2024-08-26 13:10:09 +0300

    Solution 26-08-2024 13-10

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,10 +1,30 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from taxi.models import Driver
 
 
-class DriverForm(forms.ModelForm):
-    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+class CleanLicenseNumberMixin:
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        len_license_number = len(license_number)
+        first_3 = license_number[:3]
+        first_3_chars_alpha = first_3.isalpha()
+        first_3_chars_uppercase = first_3 == first_3.upper()
+        last_5 = license_number[-5:]
+        last_5_is_digit = last_5.isdigit()
+
+        if len_license_number != 8:
+            raise ValidationError(f"Length of license number should be 8 symbols, not: {len_license_number}")
+        if not first_3_chars_alpha or not first_3_chars_uppercase:
+            raise ValidationError(f"The first 3 symbols should be uppercase letters, not: {first_3}")
+        if not last_5_is_digit:
+            raise ValidationError(f"The last 5 symbols should be digits, not: {last_5}")
+
+        return self.cleaned_data["license_number"]
+
+
+class DriverForm(forms.ModelForm, CleanLicenseNumberMixin):
 
     class Meta:
         model = Driver
@@ -15,6 +35,7 @@ class DriverForm(forms.ModelForm):
             "license_number",
             "password"
         )
+        password = forms.CharField(label="Password", widget=forms.PasswordInput)
 
     def save(self, commit=True):
         driver = super().save(commit=False)
@@ -26,7 +47,7 @@ class DriverForm(forms.ModelForm):
         return driver
 
 
-class DriverLicenseUpdateForm(forms.ModelForm):
+class DriverLicenseUpdateForm(forms.ModelForm, CleanLicenseNumberMixin):
     class Meta:
         model = Driver
         fields = ("license_number",)
